7f842e475595890773acb3eb6f96755d786a57d8
egelon <egelon@gmail.com>
2013-07-01 17:34:04 +0300

    added comment to clarify the use of the router

--- router/router.php
+++ router/router.php
@@ -5,7 +5,11 @@ class Router
      
     public function __construct() 
 	{ 
-        $this->table['controller'] = new Route('Model', 'View', 'Controller');   
+        $this->table['controller'] = new Route('Model', 'View', 'Controller');  
+		// Add routes here
+		//routes should be in the form:
+		//$this->table['  THE_VALUE_OF_GET['route']   '] = new Route('Model_for_this_route', 'the_corresponding_View_for_the_model_and_the_route', 'and_their_corresponding_Controller');  
+		
     } 
      
     public function getRoute($route) 
