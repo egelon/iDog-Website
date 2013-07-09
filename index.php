6581ee9adf34fdcebf677e0c1cda27b5f0feb0c1
Georgi Sabev <egelon@gmail.com>
2013-07-09 21:52:25 +0300

    Removed extra comments from index.php

--- index.php
+++ index.php
@@ -1,18 +1,6 @@
 <?php
 require_once('router\router.php');
 require_once('router\frontController.php');
-/*
-$model = new Model();
-//It is important that the controller and the view share the model
-$controller = new Controller($model);
-$view = new View($controller, $model);
-
-if(isset($_GET['action']) && !empty($_GET['action'])){
-    $controller->{$_GET['action']}();
-}
-
-echo $view->output();
-*/
 
 $frontController = new FrontController(new Router, $_GET['route'], isset($_GET['action']) ? $_GET['action'] : null); 
 echo $frontController->output();
