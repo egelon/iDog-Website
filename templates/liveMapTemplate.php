9992ac24d4a5f04c29915f29f3c71102dc1ede72
Georgi Sabev <egelon@gmail.com>
2013-07-11 15:36:13 +0300

    added google maps support

--- templates/liveMapTemplate.php
+++ templates/liveMapTemplate.php
@@ -6,6 +6,15 @@
   <link type="text/css" rel="stylesheet" href="css/common.css"/>
   <link type="text/css" rel="stylesheet" href="css/pageNav.css"/>
     <script type="text/javascript" src="js/jquery-1.10.0.min.js"></script>
+
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+
+    <style type="text/css">
+      html { height: 100% }
+      body { height: 100%; margin: 0; padding: 0 }
+      #map { top:50%;height: 50% }
+    </style>
+
     
 </head>
 
@@ -19,8 +28,9 @@
       </header>
         <?php echo $mainText; ?><br>
         id: <?php echo $id; ?><br>
-        lat: <?php echo $lat; ?><br>
-        lon: <?php echo $lon; ?><br>
+        lat: <div id="lat"><?php echo $lat; ?></div><br>
+        lon: <div id="lon"><?php echo $lon; ?></div><br>
+        
     </div>
 
     <footer>
@@ -28,5 +38,13 @@
     </footer>
 
   </div>
+
+
+<div id="map"></div>
+
+        <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
+        <script src="js/gmaps.js"></script>
+
+
 </body>
 </html>
