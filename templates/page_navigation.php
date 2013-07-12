c9cc3c56d4d4b562ec6d31a7789b951de8cd9eca
Georgi Sabev <egelon@gmail.com>
2013-07-13 01:47:32 +0300

    fixed logout button

--- templates/page_navigation.php
+++ templates/page_navigation.php
@@ -2,6 +2,6 @@
 	<ul>
 		<li><a href="index.php">Home</a></li>
 		<li><a href="index.php?route=profile">My profile</a></li>
-		<li><a href="#">Logout</a></li>
+		<li><a href="./register/user_logout.php">Logout</a></li>
 	</ul>
 </nav>
